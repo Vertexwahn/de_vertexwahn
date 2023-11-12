3a796a3f5286564578b9813cb08ca75ed023aae4
Vertexwahn <julian.amann@tum.de>
2023-11-12 21:49:37 +0100

    Copyright got invalid after refactoring, but was forgotton to be removed
    GitOrigin-RevId: 9735a701f9802ec3ae288b734385c83e84dc69db

--- imaging/color.h
+++ imaging/color.h
@@ -1,34 +1,8 @@
-// Copied and modified code from "appleseed" library.
-// This library is available under the MIT license. Original copyright notice:
 
-//
-// This source file is part of appleseed.
-// Visit http://appleseedhq.net/ for additional information and resources.
-//
-// This software is released under the MIT license.
-//
-// Copyright (c) 2010-2011 Francois Beaune
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-
-// Modifications are copyright by Julian Amann and available under MIT license and Apache 2.0 license
+/*
+ *  SPDX-FileCopyrightText: Copyright 2023 Julian Amann <dev@vertexwahn.de>
+ *  SPDX-License-Identifier: Apache-2.0
+ */
 
 #pragma once
 #ifndef De_Vertexwahn_Imaging_Color_0cb9a6fe_978e_4062_95e9_07dfabe40508_h
